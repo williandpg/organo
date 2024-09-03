8d40aeed36a6ef8091962ff228fa9f7ca8bc0f3c
Willian Gonçalves <willian.p.g@outlook.com>
2024-09-03 13:50:53 -0300

    Banner e Campo Texto adicionados

---
+++ src/components/CampText/CampText.js
@@ -0,0 +1,12 @@
+import './CampText.css';
+
+const CampText = () => {
+  return (
+    <div className="campo-texto">
+      <label>Nome</label>
+      <input placeholder='Digite o seu nome'/>
+    </div>
+  );
+}
+
+export default CampText;
